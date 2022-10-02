52baa4f5ce9d22a5b3a52974c8d1e4f0fc902a8c
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-10-02 09:46:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+nosex=0;
+nosey=0;
+rightwristx=0;
+leftwristx=0;
+difference=0; 
+
 function setup()
 {
 
-canvas=createCanvas(400,400);
+canvas=createCanvas(500,400);
 canvas.position(500,100);
 
 video=createCapture(VIDEO);
@@ -18,7 +24,14 @@ function gotPoses(results)
     if(results.length >0 )
     {
         console.log(results);
-        
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log("nosex = "+nosex+" and nosey = "+nosey);
+
+        rightwristx=results[0].pose.rightWrist.x;
+        leftwristx=results[0].pose.leftWrist.x;
+        difference= floor(leftwristx-rightwristx);
+        console.log("rightwristx = "+rightwristx+" and leftwristx = "+leftwristx+" difference = "+difference);
     }
 }
 
@@ -30,4 +43,8 @@ function modelLoaded()
 function draw()
 {
     background('#32a852');
+    document.getElementById("square_sides").innerHTML="width and height of a square will be = "+difference+"px";
+    fill('#4287f5');
+    stroke('#51f542');
+    square(nosex,nosey,difference);
 }
